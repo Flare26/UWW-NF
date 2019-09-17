29cfdb771394b84b1b2eaf793eff0fba04947ae4
Flare26 <47835828+Flare26@users.noreply.github.com>
2019-09-17 03:44:33 -0500

    Added python to UWW repo

---
+++ introToPython2019/CelciusToFahrenheit.py
@@ -0,0 +1,6 @@
+#Nathan Frazier Assignment 2_01
+#This program takes input and converts from C to F
+print ("Welcome to C to F converter!")
+tempC = float(input("Enter a temperature in celsius: "))
+tempF = (9/5) * tempC + 32
+print(tempC,"in Celsius =",tempF,"in Fharenheit")
---
+++ introToPython2019/JoulesCalc.py
@@ -0,0 +1,12 @@
+#Nathan Frazier Assignment 2_01
+#This program calculates the joules needed in order to heat water, takes 2 temps for input
+print("This program requires 3 input values | mass(kg), temp0(C), temp1(C)")
+print("Please enter a mass of water (kilograms)")
+waterWeight = float(input())
+print("waterWeight =",waterWeight)
+initialTemp, finalTemp = input("Please enter finalTemp & initialTemp in Celsius:").split()
+print("Initial Temperature =",initialTemp)
+print("Final Temperature =", finalTemp)
+#typecast the strings 'finalTemp' and 'initialTemp' to float() during calculation phase
+energyJ = waterWeight *  (float(finalTemp) - float(initialTemp)) * 4184
+print("It will take",energyJ,"Joules to heat",waterWeight,"kg of water from",initialTemp,"to",finalTemp,"Celsius")
