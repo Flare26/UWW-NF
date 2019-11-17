4dc879141757253f8e91cd0b011daf7e4cc7b6a2
Flare26 <47835828+Flare26@users.noreply.github.com>
2019-11-17 14:50:10 -0600

    Update hw4q4.asm

--- Assembly/hw4q4.asm
+++ Assembly/hw4q4.asm
@@ -1,4 +1,4 @@
-; Nathan Frazier Fibbonacci
+; Nathan Frazier hw 4 q4 Fibbonacci
 
 .data
     array db 15 dup(?)
@@ -10,26 +10,29 @@
           lea SI , array
           lea DI , array
           inc DI
-          mov array[SI] , 0  ; move 0 to index 0
-          mov array[DI] , 1  ; move 1 to index 0 + 1
+          mov array[SI] , 0  ; move 0 to index array[0]
+          mov array[DI] , 1  ; move 1 to index array[0 + 1]
           
-            ;right here DI is 1 index above SI
+            ; so, at this point we have db array, length 15 nulls
+            ; SI points to array[0]
+            ; DI points to array[1]
           
           
           
           
           mov CX, 13 ; 13 numbers to be generated, 0 and 1 already given
           fib:
-             mov AL , 0
-             mov BL , 0
-             mov AL , array[SI]  ; move the val at SI to AL
-             mov BL , array[DI]  ; move the val at DI to BL
-             add BL , AL    ; add AL and BL, sum will be at BL
-             inc SI ; until this point, SI has pointed to 0 now 1
-             inc DI ; until this point, DI has pointed to 1 now empty
-             mov array[DI] , BL  ; make the memory at empty DI equal to the sum at BL
-           
-                  
+             mov AL , 0   ; clear AL
+             mov BL , 0   ; clear BL
+             mov AL , array[SI]  ; first exe : copy array[0] --> AL
+             mov BL , array[DI]  ; first exe : copy array[1] --> BL
+             add BL , AL    ; arithmetic on BL : BL = AL + BL 
              
+             inc SI ; first exe : SI from array[0] to array[1]
+             inc DI ; first exe : DI from array[1] to array[2]
+              
+             mov array[DI] , BL  ; make the memory at new DI equal to the sum at BL
+                                 ; SI now points to what we just moved
+                     
           loop fib    
     
