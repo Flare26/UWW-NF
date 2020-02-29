241b8e9ee2dac9f780f8174b7b10f45a628d5038
Flare26 <47835828+Flare26@users.noreply.github.com>
2020-02-28 19:14:49 -0600

    stuff

--- introToAI/PA2/AI_PA2_ASTAR.java
+++ introToAI/PA2/AI_PA2_ASTAR.java
@@ -5,16 +5,16 @@ import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Scanner;
-import java.util.Set;
 
 // Nathan Frazier
 public class AI_PA2_ASTAR {
 // Game plan: adapt code from previous assignment to create a method that take in a file name
 // as a parameter and returns a 2D MATRIX
 	
+	private static final int INFINITY = 999999999;
 	static int gScore; // current optimal path cost from source to given vertex NUMBER MOVES IT TOOK TO GET HERE
 	static int hScore; // heuristic estimate distance from node to goal 
-	static int fScore; // gScore + hScore ( notes get dequed from open set if they have the lowest fScore)
+	static int fScore; // gScore + hScore ( notes get dequeued from open set if they have the lowest fScore)
 	
 	static String [] verts1= null;
 	static String [] verts2= null;
@@ -36,67 +36,30 @@ public class AI_PA2_ASTAR {
         }
     }
 
-    private int findLowestDistance(ArrayList<Integer> myOpenSet, double [] distances, int [] [] matrix) {
-    	//node in openSet with lowest distance
-    	// will have to check the adjacency matrix for each distance. for each node on the open set, find the neighbors and ALSO store their distances.
-    	// starting at source, among the neighbors of source > 0, find the minimum.
-    	int minDist = 0;
-    	for ( int rowIdx : myOpenSet)
-    	{
-    		for ( int colIdx : myOpenSet )
-    		{
-    			int matrixVal = matrix [rowIdx] [colIdx];
-    			if ( matrixVal < minDist && matrixVal > 0 ) {
-    				minDist = matrixVal; // if matrix value is less than current min and is NOT source OR -1
-    			} else if ( minDist == 0 && matrixVal > 0 ) {
-    				minDist = matrixVal; // if min distance has not been set and matrix val is NOT source OR -1
-    			}
-    			
-    		}
-    		
+    private ArrayList<Integer> uniformCostSearch (int source, int target, String [] verts, int [] [] matrix){
+		
+    	ArrayList<Integer> openSet = new ArrayList<Integer>();
+    	ArrayList<Integer> closedSet = new ArrayList<Integer>();
+    	double [] distances = new double [verts.length];
+    	int [] prev = new int [verts.length];
+    	
+    	for (int node = 0 ; node < verts.length ; node++) {
+    		distances[node] = Double.MAX_VALUE;
+    		prev[node] = -1;
+    	}
+    	
+    	openSet.add(source);
+    	
+    	distances[source] = 0;
+    	
+    	while ( openSet.isEmpty() == false ) {
+    		// use distances array maybe?
     		
     	}
     	
     	
+    	return null;
     }
-    
-	private ArrayList<Integer> dijkstras(int sourceIdx, int targetIdx, String [] verts, int [] [] matrix) {
-		
-		ArrayList<Integer> openSet = new ArrayList<Integer> ();
-		//ArrayList<Integer> closedSet = new ArrayList<Integer> ();
-		double [] distances = new double [verts.length]; //Each index 'i' represents the current minimum path cost for node 'i'
-		int [] prev = new int [verts.length]; 
-		
-		
-		//Initially, we don't know the optimal path cost or the parent of any node
-		//To represent a missing node, we'll use -1 as a placeholder
-		//Add all nodes in the graph to the open set
-		for ( int nodeIdx = 0 ; nodeIdx < verts.length ; nodeIdx++ ) {
-			distances [nodeIdx] = Double.POSITIVE_INFINITY; // double just so happens to support positive infinity
-			prev [nodeIdx] = -1;
-			openSet.add(nodeIdx);
-		}
-		
-		// Distance from starting point to starting point will be 0
-		distances[sourceIdx] = 0;
-		
-		// EXPLORE
-		
-		while ( ! openSet.isEmpty() ) {
-			// Check open set for lowest distance among them
-			int current = findLowestDistance(openSet, matrix);
-			
-			
-			
-			
-			
-		}
-		
-		
-		
-		return new ArrayList<Integer> ();
-				
-	}
 	
 	private void AStar() {
 		
@@ -200,6 +163,14 @@ class Populator {
 		}
 		matrixReader.close();
 		return adj_matrix;
+	}
+	
+	// Node class pleas and thank you
+	class Node {
+		int idx;
+		String label;
+		
+		
 	}
 	
 }
