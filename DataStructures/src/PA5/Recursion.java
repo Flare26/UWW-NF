76f47814d2d2b49f4cd8465782899e5e648d1001
Flare26 <47835828+Flare26@users.noreply.github.com>
2020-03-29 17:33:47 -0500

    Recursion complete

--- DataStructures/src/PA5/Recursion.java
+++ DataStructures/src/PA5/Recursion.java
@@ -6,10 +6,10 @@ public class Recursion {
 		
 		if ( n % 2 == 0 ) {
 			return sumEvenDigits(n/10) + n%10;
-		}
-		 	// nothing gets added if odd
+		} else {
+		 	// nothing gets added if odd, just pass in the next number set by div by 10. This effectively moves decimal place left
 			return sumEvenDigits(n/10);
-
+		}
 		}
 
 	public static void binaryStringsWithMoreOnes(int n) {
@@ -17,5 +17,13 @@ public class Recursion {
 	}
 
 	private static void binaryStringsWithMoreOnes(String str, int numZeroes, int numOnes, int n) { // complete this function  
+		if ( str.length() == n && numZeroes < numOnes )
+			System.out.println(str);
+		else if ( str.length() < n ) {
+			// Method will run again with appending 0. It will end up running both cases
+			binaryStringsWithMoreOnes(str + "0" , numZeroes + 1, numOnes, n);
+			binaryStringsWithMoreOnes(str + "1" , numZeroes, numOnes + 1, n);
+		}
+		
 	}
 }
